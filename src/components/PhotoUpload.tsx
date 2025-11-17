5eaf3459d239897e2417a1b9c99041bb8957def7
salasvega <salasvega@gmail.com>
2025-11-17 23:21:45 +0100

    Updated PhotoUpload.tsx

--- src/components/PhotoUpload.tsx
+++ src/components/PhotoUpload.tsx
@@ -306,7 +306,7 @@ export function PhotoUpload({ photos, onPhotosChange, maxPhotos = 8, onAnalyzeCl
               ) : (
                 <>
                   <Sparkles className="w-5 h-5" />
-                  Analyser avec l'IA
+                  Analyse et Création - IA
                 </>
               )}
             </button>
