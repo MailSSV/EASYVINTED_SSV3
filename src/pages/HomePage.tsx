633fd73b7b4d5412f6c96d1219650b5662d35c23
MailSSV <monadressemailssv1@gmail.com>
2025-11-20 15:48:50 +0100

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Sparkles, sparkles, Calendar, Upload } from "lucide-react";
+import { Sparkles, WandSparkles, Calendar, Upload } from "lucide-react";
 
 export function HomePage() {
   return (
