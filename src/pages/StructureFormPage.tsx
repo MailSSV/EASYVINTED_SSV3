7af0a03751bfdce081a110ab7d54995c098f60f6
MailSSV <monadressemailssv1@gmail.com>
2025-11-22 12:58:58 +0100

    Updated StructureFormPage.tsx

--- src/pages/StructureFormPage.tsx
+++ src/pages/StructureFormPage.tsx
@@ -227,6 +227,7 @@ export function StructureFormPage() {
           <li>Glissez-Déposez vos Photos dans la section "+ Ajoute des Photos" de Vinted </li>
           <li>Cliquez sur le bouton Copier pour chaque champ</li>
           <li>Collez la valeur dans le champ correspondant sur Vinted</li>
+           <li>Collez la valeur dans le champ correspondant sur Vinted</li>
            
               </ol>
         <div className="flex flex-col gap-2 mt-3">
